3d60879d4527b7f0c140c0ed38195f97915d378f
ArturAhmetshin <arturahm@mail.ru>
2017-08-23 22:53:29 +0300

    ver4

--- src/main/java/ru/ahmetshin/test/DAO/RoleDAO.java
+++
@@ -1,8 +0,0 @@
-package ru.ahmetshin.test.DAO;
-
-import ru.ahmetshin.test.domain.Roles;
-
-public interface RoleDAO {
-    public Roles getRoleById(long id);
-    public void addRole(Roles role);
-}
